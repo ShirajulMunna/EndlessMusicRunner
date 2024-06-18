fbbecd3a62c1564c8029b6ec31d85fc7a2b4317f
leehyoenjong <99190990+leehyoenjong@users.noreply.github.com>
2024-06-18 10:59:55 +0900

    키 판정 수정
    키 판정 수정

--- Assets/@Scripts/Entity/Monster/Monster.cs
+++ Assets/@Scripts/Entity/Monster/Monster.cs
@@ -46,6 +46,10 @@ public class Monster : Entity, IMonsterMove
     {
         get => GameManager.instance.player;
     }
+    protected IPlayer_State Player_State
+    {
+        get => GameManager.instance.player.M_State;
+    }
     public UniqMonster uniqMonster;
 
     protected virtual void Start()
